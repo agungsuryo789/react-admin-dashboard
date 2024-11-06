c43bb5f9855f3dff71df48d99391004125e6e55a
Agung Suryo <agungsuryo645@gmail.com>
2024-11-06 09:23:12 +0800

    Slicing Login Page
    - add routing
    - add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid grid-cols-5">
+        <div className="justify-items-center content-center col-span-3 bg-blue-800 p-4 w-full h-svh text-left">
+          <h2 className="font-bold text-4xl">GoFinance</h2>
+          <p className="my-2">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
+          <button className="my-2">Read More</button>
+        </div>
+        <div className="justify-items-center content-center col-span-2 bg-white p-4 w-full h-svh text-black text-left">
+          <h3 className="font-bold text-4xl">Hello Again</h3>
+          <p className="my-2">Welcome back</p>
+          <div className="flex flex-col">
+            <input type="email" className="bg-white my-2" placeholder="email" />
+            <input
+              type="password"
+              className="bg-white my-2"
+              placeholder="password"
+            />
+          </div>
+          <div className="flex flex-col">
+            <button className="my-2">Login</button>
+            <button className="my-2">forgot password</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
